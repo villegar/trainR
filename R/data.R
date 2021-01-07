44b49c1f6cbc41998d33bc67e4059e760f717ebd
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-01-07 15:55:44 +0000

    Add dataset documentation

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' National Rail Enquiries (NRE) Station Codes dataset
+#'
+#' A dataset containing information of 1972 British National Rail Enquiries
+#' (NRE) station 3 letter alpha (CRS) codes.
+#'
+#' @format A data frame with 1972 rows and 2 variables:
+#' \describe{
+#'     \item{name}{Station name.}
+#'     \item{crs}{Station code.}
+#' }
+#' @author National Rail Enquiries (NRE)
+#' @usage data(station_codes)
+#' @keywords datasets
+#' @source \url{https://www.nationalrail.co.uk/stations_destinations/48541.aspx}
+"station_codes"
