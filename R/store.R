c5468b1033a4dc3c4d9148860e57583202797dfd
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-01-25 10:03:48 +0000

    Add store.busServices generic method

--- R/store.R
+++ R/store.R
@@ -48,8 +48,67 @@ store.StationBoard <- function(x, ..., file) {
     openxlsx::writeData(wb = wb, sheet = "StationBoard")
   openxlsx::addWorksheet(wb, "trainServices")
   if (all(!is.na(x$trainServices)))
-      store(x$trainServices, wb = wb)
+    store(x$trainServices, wb = wb)
+  openxlsx::addWorksheet(wb, "busServices")
+  if (all(!is.na(x$busServices)))
+    store(x$busServices, wb = wb)
   openxlsx::saveWorkbook(wb, file = file)
+}
+
+#' @param wb Workbook object, created with
+#'     \code{\link[openxlsx:createWorkbook]{openxlsx::createWorkbook}}.
+#' @rdname store
+#' @export
+store.busServices <- function(x, ..., wb) {
+  # Local bindings
+  . <- crs <- destination <- locationName <- origin <- NULL
+  previousCallingPoints <- subsequentCallingPoints <- value <- NULL
+
+  services <- x[[1]] %>%
+    dplyr::mutate(origin_crs = purrr::map(origin, "crs"),
+                  origin_via = purrr::map(origin, "via"),
+                  dest_crs = purrr::map(destination, "crs"),
+                  dest_via = purrr::map(destination, "via")) %>%
+    dplyr::select(-c(origin,
+                     destination,
+                     previousCallingPoints,
+                     subsequentCallingPoints)) #%>%
+  openxlsx::writeData(wb, "busServices", services)
+  # openxlsx::write.xlsx(file = file, sheetName = "busServices", row.names = FALSE, overwrite = FALSE)
+  openxlsx::addWorksheet(wb, "busCallingPoints")
+
+  # Find services without previous and subsequential calling points
+  idx <- is.na(x[[1]]$previousCallingPoints) & is.na(x[[1]]$subsequentCallingPoints)
+  x[[1]] <- x[[1]][!idx, ]
+  if (any(!is.na(x[[1]]$previousCallingPoints)) |
+      any(!is.na(x[[1]]$subsequentCallingPoints)))
+    purrr::map_df(x[[1]]$serviceID,
+                  function(serviceID) {
+                    prev <- x[[1]]$previousCallingPoints[x[[1]]$serviceID == serviceID]
+                    subs <- x[[1]]$subsequentCallingPoints[x[[1]]$serviceID == serviceID]
+                    if (any(!is.na(prev)) | any(!is.na(subs))) {
+                      aux <- list(prev[[1]], subs[[1]])
+                      names(aux) <- rep(serviceID, 2)
+                      aux <- aux %>%
+                        tibble::enframe(., name = "serviceID") %>%
+                        tidyr::unnest(value) %>%
+                        dplyr::filter(!is.na(locationName) & !is.na(crs))
+                      if ("value" %in% colnames(aux))
+                        aux <- aux %>%
+                        dplyr::select(-value)
+                      aux
+                    } else {
+                      NA
+                    }
+                  }) %>%
+    openxlsx::writeData(wb = wb, sheet = "busCallingPoints", ...)
+
+  # openxlsx::addWorksheet(wb, "previousCallingPoints")
+  # if (any(!is.na(x[[1]]$previousCallingPoints)))
+  #   store(x[[1]]$previousCallingPoints, wb = wb, serviceID = x[[1]]$serviceID)
+  # openxlsx::addWorksheet(wb, "subsequentCallingPoints")
+  # if (any(!is.na(x[[1]]$subsequentCallingPoints)))
+  #   store(x[[1]]$subsequentCallingPoints, wb = wb, serviceID = x[[1]]$serviceID)
 }
 
 #' @param wb Workbook object, created with
