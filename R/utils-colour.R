331a56935cb81a549e9f665962751bc95b89ed43
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-01-29 21:54:01 +0000

    Add internal function to add change colour of string based on scheduled and expected times: On time (green), Delayed (yellow), Cancelled (red), early (green), delayed by 5 minutes or less (yellow), otherwise red.

---
+++ R/utils-colour.R
@@ -0,0 +1,37 @@
+
+#' Add colour to string
+#' Add colour to string based on scheduled (`time`) and `expected` time of a
+#' service.
+#'
+#' @param time String with service scheduled time, format: `%H:%M`.
+#' @param expected String with service expected time, format: `%H:%M`.
+#' @inheritParams print_board
+#'
+#' @return
+#' @keywords internal
+colour_time <- function(time, expected, show_colours = TRUE) {
+  if (!show_colours)
+    return(expected)
+  purrr::map2_chr(time, expected,
+                  .colour_time)
+}
+
+#' @keywords internal
+.colour_time <- function(time, expected) {
+  if (tolower(expected) == "on time")
+    return(crayon::green(expected))
+  if (tolower(expected) == "delayed")
+    return(crayon::yellow(expected))
+  if (tolower(expected) == "cancelled")
+    return(crayon::red(expected))
+
+  minutes <- difftime(strptime(expected, format = "%H:%M"),
+                      strptime(time, format = "%H:%M"),
+                      units = "mins")
+  if (minutes <= 0)
+    return(crayon::green(expected))
+  if (minutes <= 5)
+    return(crayon::yellow(expected))
+  if (minutes > 5)
+    return(crayon::red(expected))
+}
