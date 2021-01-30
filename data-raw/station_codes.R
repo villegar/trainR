d4b4de236c212587dea4e89bb9ab7b5a633b0946
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-01-30 16:19:08 +0000

    Add check for duplicated entries

--- data-raw/station_codes.R
+++ data-raw/station_codes.R
@@ -1,5 +1,4 @@
 ## code to prepare the `station_codes` dataset
-
 filename <- "https://www.nationalrail.co.uk/station_codes%20(07-12-2020).csv"
 station_codes <- readr::read_csv(filename)
 names(station_codes) <- rep(c("name", "crs"), 4)
@@ -9,7 +8,17 @@ station_codes <- station_codes %>%
   tidyr::unnest(cols = c("name", "crs")) %>%
   dplyr::arrange(name)
 
+# Find rows with NAs
 station_codes <- station_codes[!is.na(station_codes$name) &
                                !is.na(station_codes$crs), ]
+
+# Find duplicates
+idx <- which(duplicated(station_codes$crs))
+if (length(idx) > 0) {
+  # Show duplicated CRS codes
+  station_codes[station_codes$crs %in% station_codes$crs[idx], ]
+  # Remove duplicated
+  station_codes <- station_codes[-idx, ]
+}
 usethis::use_data(station_codes, overwrite = TRUE)
 
