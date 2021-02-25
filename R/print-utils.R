de4a96a330b9c0ee542b06da1a0d13abda421b18
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-02-25 18:09:53 +0000

    Format .print_service_details

--- R/print-utils.R
+++ R/print-utils.R
@@ -11,7 +11,13 @@
              "{ifelse(!is.na(via) & via != '', '\n', '')}")
 }
 
-.print_service_details <- function(expected, platform, station, time, via, show_colours, ...) {
+.print_service_details <- function(expected,
+                                   platform,
+                                   station,
+                                   time,
+                                   via,
+                                   show_colours,
+                                   ...) {
   if (length(station) > 1 & length(via) > 1) { # Services with multiple locations
     locations <- paste0(station, collapse = " and ")
     if (length(unique(via)) == 1) {
@@ -24,12 +30,24 @@
                              via = via[length(via)],
                              show_colours = show_colours))
       }
-    }
-    aux <- .glue_details(expected, platform, station[1], time, via[1], show_colours)
+    } # else {
+      # # Handle services with multiple destinations and different via
+    # }
+    aux <- .glue_details(expected = expected,
+                         platform = platform,
+                         station = station[1],
+                         time = time,
+                         via = via[1],
+                         show_colours = show_colours)
     paste0(aux,
            purrr::map_chr(seq_along(station)[-1],
                           function(i) {
-                            .glue_details(NA, NA, paste("and", station[i]), NA, via[i], show_colours)
+                            .glue_details(expected = NA,
+                                          platform = NA,
+                                          station = paste("and", station[i]),
+                                          time = NA,
+                                          via = via[i],
+                                          show_colours = show_colours)
                           }),
            collapse = "\n")
   } else { # Services with single location
